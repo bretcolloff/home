2340825e0e0d96b292233b00eb1b3ccdc264455c
bretcolloff <bret.colloff@gmail.com>
2016-08-24 13:58:33 +0100

    Create trie.py

---
+++ trie.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, character, last):
+        self.isWord = last
+        self.children = []
+        self.character = character
+
+class Trie:
+    def __init__(self):
+        self.root = Node('', False)
+
+    # Insert a word
+    def insert(self, word):
+        chars = list(word)
+
+        p = self.root
+        for char in chars:
+            found = False
+            # see if the children container the character.
+            for child in p.children:
+                if child.character == char:
+                    found = True
+                    p = child
+                    break
+
+            if not found:
+                # we didn't find the character, lets add it.
+                end = char == chars[-1]
+                newNode = Node(char, end)
+                p.children.append(newNode)
+                p = newNode
+
+    def lookup(self, word):
+        chars = list(word)
+
+        p = self.root
+        for char in chars:
+            found = False
+            for child in p.children:
+                if child.character == char:
+                    found == True
+                    if char == chars[-1]:
+                        return (True, child.isWord)
+                    p = child
+
+                if child == p.children[-1]:
+                    return (False, False)
+
+def main(argv=None):
+    t = Trie()
+    print ("insert", "word", "worm", "working")
+    t.insert("word")
+    t.insert("worm")
+    t.insert("working")
+    print ("worki", t.lookup("worki"))
+    print ("work", t.lookup("work"))
+    print ("worm", t.lookup("worm"))
+
+if __name__ == '__main__':
+    main()
